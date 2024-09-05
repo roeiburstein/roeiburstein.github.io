0b739694e6e5ebf3728e90263f1e34512fb6e869
Roei Burstein <roei.burstein@gmail.com>
2024-09-04 20:48:21 -0700

    More updates

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -14,24 +14,6 @@ function Footer() {
           <p className="text-sm">
             © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/roeiburstein/" className="text-[#16f2b3]">Roei Burstein</Link>
           </p>
-          <div className="flex items-center gap-5">
-            <Link
-              target="_blank"
-              href="https://github.com/said7388/developer-portfolio"
-              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
-            >
-              <IoStar />
-              <span>Star</span>
-            </Link>
-            <Link
-              target="_blank"
-              href="https://github.com/said7388/developer-portfolio/fork"
-              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
-            >
-              <CgGitFork />
-              <span>Fork</span>
-            </Link>
-          </div>
         </div>
       </div>
     </div >
