07b86528bd3002650e46e5c366a940dd0e610491
Roei Burstein <roei.burstein@gmail.com>
2024-09-04 20:44:12 -0700

    More changes

--- app/layout.js
+++ app/layout.js
@@ -10,9 +10,9 @@ import ScrollToTop from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Abu Said - Software Developer",
+  title: "Roei Burstein's portfolio - Software Engineer",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Roei Burstein. I am a full stack developer and a self taught developer. I am a Software Engineer with a focus on backend systems in a variety of programming languages.",
 };
 
 export default function RootLayout({ children }) {
